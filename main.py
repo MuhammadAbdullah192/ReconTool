ebf3b6a0e95010a9d7ea37b069fc3d7eccb98e5e
MuhammadAbdullah192 <156157620+MuhammadAbdullah192@users.noreply.github.com>
2025-07-26 01:38:35 +0500

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ import portscan
 import banner_grabber
 import tech_detector
 import sys
+from pyfiglet import Figlet
+from rich.console import Console
+from rich.panel import Panel
 
+console = Console()
+figlet = Figlet(font='slant')  # You can also try 'standard', 'block', 'doom', etc.
+banner_text = figlet.renderText("Shadow X")
+
+console.print(Panel.fit(banner_text, title=" Recon Tool ", subtitle="Version 1.0", style="bold cyan"))
 def interactive_menu():
     while True:
         print("\nMain Menu:")
