394051c84e86a9ccbc4f66dc5c368cd37bec4e0c
MuhammadAbdullah192 <156157620+MuhammadAbdullah192@users.noreply.github.com>
2025-07-25 15:44:42 +0500

    tech_detector.py

--- tech_detector.py
+++ tech_detector.py
@@ -2,7 +2,7 @@ import os
 from logger import logger
 from datetime import datetime
 
-def tech_detect_whatweb():
+def tech_detect_whatweb(domain):
     url = input("Enter URL (e.g., https://example.com): ").strip()
 
     if not url.startswith("http"):
